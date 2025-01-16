1a20767e19402889042cea6c1b44000b729a68ea
AgrippinoC <128165350+AgrippinoC@users.noreply.github.com>
2025-01-16 10:52:06 +0100

    Update Prenotazione.java

--- Codice/Prenotazione.java
+++ Codice/Prenotazione.java
@@ -62,7 +62,7 @@ public class Prenotazione {
 
 	@Override
 	public String toString() {
-		return "Prenotazione di " + CartaPrenotazione.IDcarta +", " + IDPrenotazione + ": " + Data + ", " + Ora + ".\nGioco:" + GiocoPrenotato.IDgioco + ", numero giocatori=" + numGiocatori + '.\n';
+		return "Prenotazione di " + CartaPrenotazione.IDcarta +", " + IDPrenotazione + ": " + Data + ", " + Ora + ".\nGioco:" + GiocoPrenotato.getCodice() + ", numero giocatori=" + numGiocatori + '\n';
 	}
 
 }
